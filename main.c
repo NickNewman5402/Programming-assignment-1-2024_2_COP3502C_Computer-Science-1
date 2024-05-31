7d2f36e116e30715a444f51b7eff844a5e55bd77
Nicholas Newman <33791854+NickNewman5402@users.noreply.github.com>
2024-05-31 01:04:45 +0000

    Final submission pending whether or not making struct item portions, struct recipe totalPortions, and unitInPounds in calculate order a double is ok.

--- main.c
+++ main.c
@@ -5,7 +5,10 @@
 //Student Info
 /*
   COP3502C Assigment 1
-  This program is written by: Nicholas Eric Newman
+  This program is written by: 
+  Nicholas Eric Newman
+  Due date June 2, 2024
+  student ID 5292926
   */
   // Overview
   /*
@@ -39,41 +42,65 @@
   Given a list of possible smoothie ingredients, a list of smoothie recipes, and lists of sales from several stores,
   determine how much of each ingredient each store must order.
 
+  Sample Input:															Sample Output:
+  8// ingredients														List of items for store 1:
+  yogurt// ingrdient[0]													yogurt 8.444444
+  chocolate																chocolate 4.444444
+  raspberry																banana 2.222222	
+  banana																	mango 12.888889
+  mango																	spinach 2.000000
+  spinach								
+  kiwi																List of items for store 2:
+  strawberry																yogurt 6.000000
+  3// 3 recipes															banana 3.000000
+  4 7 2 3 1 6 1 0 2// 4 ingrdients ingrdient[7] portion(2) etc...			kiwi 3.000000
+  3 0 2 5 1 4 2															strawberry 6.000000
+  4 0 2 4 4 3 1 1 2					
+  3// 3 stores														List of items for store 3:
+  2 1 10 2 20// 2 recipes, recipe 1, weight of 10, recipe 2 weight 20		yogurt 8.666667
+  1 0 18																	chocolate 3.333333
+  3 0 10 1 5 2 15															banana 3.333333
+                                      mango 8.666667
+                                      spinach 1.000000	
+                                      kiwi 1.666667
+                                      strawberry 3.333333
+
   */
 
 
 typedef struct item {
   int itemNo;
-  double portions;
+  double portions;// made double as leaving it as int did not produce a double in the end
 } item;
 
 typedef struct recipe {
   int itemCount;
   item* itemList;
-  double totalPortions;
+  double totalPortions;// made double as leaving it as int did not produce a double in the end
 } recipe;
 
-char** readIngredients(int *numIngredients);// done
-recipe* readRecipe();// done
-recipe** readAllRecipes(int *numRecipes);// done
+char** readIngredients(int *numIngredients);
+recipe* readRecipe();
+recipe** readAllRecipes(int *numRecipes);
 double* calculateOrder(int ingredientCount, int numSmoothies, recipe** recipeList);
 void printOrder(char** ingredientNames, double* orderList, int numIngredients);
 void freeIngredients(char** ingredientList, int numIngredients);
 void freeRecipes(recipe** allRecipes, int numRecipes);
 
 
-// Confirmed to be good as of 5/27/24 @ 10:37am
+// puts ingredients in a char array to be refrenced later 
 char **readIngredients(int *numIngredients) {
 
-  printf("Enter number of ingrdients: ");
   scanf("%d", numIngredients);
   char name[21];// creates a char array of size 20 allowing the \0 at the end of a string
-  char **ingredients = (char**)malloc(sizeof(char*)*(*numIngredients)); // creates a pointer to numIngredient pointers. 
+  char **ingredients = (char**)malloc(sizeof(char*)*(*numIngredients));  
+
   for (int i = 0; i < *numIngredients; i++) {
-    printf("Enter ingredient [%d] name: ", i + 1);
+
     scanf("%s", name);
-    ingredients[i] = (char*)malloc((strlen(name) + 1) * sizeof(char));// creates a box of size of the name + 1 (for the \0) * the size of char that is pointed to by ingredients[i].
-    strcpy(ingredients[i], name);// populates ingredients[i] with the name entered.
+    ingredients[i] = (char*)malloc((strlen(name) + 1) * sizeof(char));
+    strcpy(ingredients[i], name);// populates ingredients[i] with the names entered.
+
   }
 
   return ingredients;
@@ -83,11 +110,13 @@ char **readIngredients(int *numIngredients) {
 // takes in number of recipes and then runs the function readRecipe
 recipe** readAllRecipes(int *numRecipes) {
 
-  printf("\nEnter number of smoothie recipes: ");
   scanf("%d", numRecipes);
+
   recipe **smoothies = (recipe**)malloc(*numRecipes * sizeof(recipe*));
   for (int i = 0; i < *numRecipes; i++) {
+
     smoothies[i] = readRecipe(); // returns a recipe pointer
+
   }
 
   return smoothies;
@@ -97,29 +126,22 @@ recipe** readAllRecipes(int *numRecipes) {
 // Creates a smoothie with ingrdient info and ratio info saved in smootie[i] created in readAllRecepies
 recipe *readRecipe() {
 
-  // sample input:
-  /*
-    m I1 R1 I2 R2 … Im Rm
-    m represents the number of different ingredients in the smoothie (1 ? m ? 100)
-    I1 represents the ingredient number of the first ingredient (0 ? I1 < n)
-    R1 represents the number of parts (ratio) of the first ingredient (1 ? R1 ? 1000) in the smoothie recipe
-
-    The rest of the I and R variables represent the corresponding information for the rest of the smoothie ingredients.
-  */
 
   recipe *smoothie = (recipe*)malloc(sizeof(recipe));
   int numItems;
-  printf("\nEnter number of items: ");
+
   scanf("%d", &numItems);
+
   smoothie->itemCount = numItems;
+
   smoothie->itemList = (item*)malloc(numItems * sizeof(item));
   smoothie->totalPortions = 0;
 
   for (int i = 0; i < numItems; i++) {
-    printf("Enter item [%d] name: ", i);
-    scanf("%d", &smoothie->itemList[i].itemNo);// item number is storing the number to be referenced later by infredients[i]? Im
-    printf("Enter ratio amout of item [%d]: ", i);
-    scanf("%lf", &smoothie->itemList[i].portions);// Rm
+
+    scanf("%d", &smoothie->itemList[i].itemNo);
+    scanf("%lf", &smoothie->itemList[i].portions);// made %lf to reflect the double data type
+
     smoothie->totalPortions += smoothie->itemList[i].portions;
   }
 
@@ -128,26 +150,29 @@ recipe *readRecipe() {
 
 }
 
-double* calculateOrder(int ingredientCount/*8*/, int numSmoothies/*3*/, recipe** recipeList/*smoothieList*/) {
+double* calculateOrder(int ingredientCount, int numSmoothies, recipe** recipeList) {
 
   double* amountOfEachItem = (double*)calloc(ingredientCount, sizeof(double));
   int numOfRecipes;
   int recipeNum;
-  double unitInPounds;
+  double unitInPounds;// made double as leaving it as int did not produce a double in the end
 
-  printf("How many recipes: ");//debug
   scanf("%d", &numOfRecipes);
 
 
   for (int i = 0; i < numOfRecipes; i++) {
-    printf("order #%d Which recipe: ", i+1);//debug
+
     scanf("%d", &recipeNum);
-    printf("order #%d Unit in pounds: ", i +1);//debug
-    scanf("%lf", &unitInPounds);
+    scanf("%lf", &unitInPounds);// made %lf to reflect the double data type
+
     for (int j = 0; j < recipeList[recipeNum]->itemCount; j++) {
-        amountOfEachItem[recipeList[recipeNum]->itemList[j].itemNo] += (unitInPounds * recipeList[recipeNum]->itemList[j].portions) / recipeList[recipeNum]->totalPortions;
-      }
+
+        amountOfEachItem[recipeList[recipeNum]->itemList[j].itemNo] 
+        += (unitInPounds * recipeList[recipeNum]->itemList[j].portions) 
+        / recipeList[recipeNum]->totalPortions;
+
     }
+  }
 
   return amountOfEachItem;
 }
@@ -155,11 +180,34 @@ double* calculateOrder(int ingredientCount/*8*/, int numSmoothies/*3*/, recipe**
 void printOrder(char** ingredientNames, double* orderList, int numIngredients) {
 
   for (int i = 0; i < numIngredients; i++) {
+
     if (orderList[i] > 0) {
+
       printf("%s %.6f\n", ingredientNames[i], orderList[i]);
+
     }
   }
   printf("\n");
+}
+
+void freeIngredients(char** ingredientList, int numIngredients) {
+
+  for (int i = 0; i < numIngredients; i++) {
+    free(ingredientList[i]);
+  }
+
+  free(ingredientList);
+}
+
+void freeRecipes(recipe** allRecipes, int numRecipes) {
+
+  for (int i = 0; i < numRecipes; i++) {
+    free(allRecipes[i]->itemList);
+    free(allRecipes[i]);
+  }
+
+  free(allRecipes);
+
 }
 
 int main(void) {
@@ -175,19 +223,22 @@ int main(void) {
   recipe **recipeList;
 
 
-  ingredientNames = readIngredients(&numIngredients);
+  ingredientNames = readIngredients(&numIngredients);//readIngredients returns char **
 
-  smoothieList = readAllRecipes(&numRecipes);
+  smoothieList = readAllRecipes(&numRecipes);// readAllRecipes returns recipe **
 
-  printf("How many stores: ");
   scanf("%d", &numStores);
   for (int i = 0; i < numStores; i++) {
-    amountOfEachItem = calculateOrder(numIngredients, numRecipes, smoothieList);
-    // I can not get the amount of each item back
+
+    amountOfEachItem = calculateOrder(numIngredients, numRecipes, smoothieList);// calculateOrder returns double *
     printf("List of items for store %d:\n", i+1);
     printOrder(ingredientNames, amountOfEachItem, numIngredients);
+    free(amountOfEachItem);
+
   }
 
+  freeIngredients(ingredientNames, numIngredients);
+  freeRecipes(smoothieList, numRecipes);
 
   return 0;
 }
